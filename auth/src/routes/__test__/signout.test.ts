db50d82987d2ef9a00e90895d699a9364fe0aa87
liran <liran@gmail.com>
2025-09-04 15:03:07 +0300

    last min cahge

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -1,21 +1,21 @@
-import request from "supertest";
-import { app } from "../../app";
+import request from 'supertest';
+import { app } from '../../app';
 
-it("clears the cookie after signing out", async () => {
+it('clears the cookie after signing out', async () => {
   await request(app)
-    .post("/api/auth/users/signup")
+    .post('/api/auth/users/signup')
     .send({
-      email: "test@test.com",
-      password: "password",
+      email: 'test@test.com',
+      password: 'password',
     })
     .expect(201);
 
   const response = await request(app)
-    .post("/api/auth/users/signout")
+    .post('/api/auth/users/signout')
     .send({})
     .expect(200);
 
-  expect(response.get("Set-Cookie")![0]).toEqual(
-    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; samesite=lax; httponly"
+  expect(response.get('Set-Cookie')![0]).toEqual(
+    'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; samesite=lax; httponly'
   );
 });
